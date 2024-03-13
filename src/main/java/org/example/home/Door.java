4002aafa7e4fc5b456c70eccd02bddf829c01cc7
avk1702 <avk1702@bk.ru>
2024-03-13 15:58:11 +0500

    3

---
+++ src/main/java/org/example/home/Door.java
@@ -0,0 +1,19 @@
+package org.example.home;
+
+/**
+ * Класс для описания двери
+ */
+public class Door {
+
+    private boolean isPrize;
+
+    public Door(boolean isPrize) {
+        this.isPrize = isPrize;
+    }
+
+    public boolean isPrize() {
+        return isPrize;
+    }
+}
+
+
